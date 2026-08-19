38761a8c6c17bcbd8f68851e90ee5d7c0942da9b
Ali Özgür <aliozgur@trex.com.tr>
2026-08-19 19:31:23 +0300

    Add a thank-you page after feedback is sent.

--- astro.config.js
+++ astro.config.js
@@ -7,6 +7,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://querial.github.io',
   base: '/',
+  redirects: {
+    "/thankyou": "/thank-you",
+  },
   integrations: [
     react(),
     tailwind({
